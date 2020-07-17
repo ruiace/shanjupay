736c670422ebcaeabadc40c4be2a05ffef439612
ruipeng <ruiace@126.com>
2020-07-18 07:52:37 +0800

    第三天结束2

---
+++ test-rocketmq/src/main/java/com/rp/rocket/test/App.java
@@ -0,0 +1,19 @@
+package com.rp.rocket.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author rp
+ * @create 2020/7/18
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
